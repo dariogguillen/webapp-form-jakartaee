b603dde04ea464c4e4274262be1e3ea4febeac09
Darío Guillén <guillendario@gmail.com>
2025-04-14 16:04:02 -0600

    some input select added

--- src/main/java/org/dgg/webapp/form/FormServlet.java
+++ src/main/java/org/dgg/webapp/form/FormServlet.java
@@ -8,6 +8,8 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 @WebServlet("/registro")
 public class FormServlet extends HttpServlet {
@@ -17,6 +19,9 @@ public class FormServlet extends HttpServlet {
         String username = req.getParameter("username");
         String password = req.getParameter("password");
         String email = req.getParameter("email");
+        String pais = req.getParameter("pais");
+        String[] lenguajes = req.getParameterValues("lenguajes");
+        String[] roles = req.getParameterValues("roles");
         try (PrintWriter out = resp.getWriter()) {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
@@ -28,8 +33,19 @@ public class FormServlet extends HttpServlet {
             out.println("<h1>Resultado Form</h1>");
             out.println("<ul>");
             out.println("<li>Username: " + username + "</li>");
-            out.println("<li>Password" + password + "</li>");
-            out.println("<li>Email" + email + "</li>");
+            out.println("<li>Password: " + password + "</li>");
+            out.println("<li>Email: " + email + "</li>");
+            out.println("<li>País: " + pais + "</li>");
+            out.println("<li>Lenguajes: <ul>");
+            Arrays.asList(lenguajes).forEach(l -> {
+                out.println("<li>" + l + "</li>");
+            });
+            out.println("</ul></li>");
+            out.println("<li>Roles: <ul>");
+            Arrays.asList(roles).forEach(r -> {
+                out.println("<li>" + r + "</li>");
+            });
+            out.println("</ul></li>");
             out.println("</ul>");
             out.println("</body>");
             out.println("</html>");
